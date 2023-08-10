0c2c63c8e5c04ee2fbbba488b1e07db23020a86d
Ouail Amarir <ouailamarir@gmail.com>
2023-08-10 20:39:21 +0200

    ajout page admin avec bouton modal + léger changement de design et de couleur du site

--- epilogue/assets/js/script.js
+++ epilogue/assets/js/script.js
@@ -37,3 +37,63 @@ function closeMenuOutsideClick(event) {
         document.removeEventListener("click", closeMenuOutsideClick);
     }
 }
+
+// JavaScript en bas de votre page
+document.addEventListener("DOMContentLoaded", function () {
+    const addButton = document.getElementById("add-project-button");
+    const modal = document.getElementById("add-project-modal");
+    const closeButton = document.querySelector(".close");
+    const projectForm = document.getElementById("project-form");
+  
+    addButton.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+  
+    closeButton.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  
+    window.addEventListener("click", (event) => {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  
+    projectForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+    
+        const projectName = document.getElementById("project-name").value;
+        const projectDescription = document.getElementById("project-description").value;
+        const projectImage = document.getElementById("project-image").files[0];
+        const projectImageURL = URL.createObjectURL(projectImage);
+        const projectImageElement = document.createElement("img");
+    
+        // Créez un nouvel élément article avec les données du formulaire et de l'image
+        const newProject = document.createElement("article");
+        newProject.classList.add("item");
+        newProject.innerHTML = `
+            <header>
+                <a href="#" style="background-image: url('${projectImageURL}');"><img src="./images/placeholder-image.png" alt="" /></a>
+                <h3>${projectName}</h3>
+            </header>
+            <p>${projectDescription}</p>
+            <ul class="actions">
+                <li><a href="#" class="button">More</a></li>
+            </ul>
+        `;
+    
+        // Afficher l'image dans la modal (avant d'ajouter le projet)
+
+        projectImageElement.src = projectImageURL;
+        newProject.querySelector("a").appendChild(projectImageElement);
+    
+        // Ajoutez le nouvel élément à la section des projets
+        const projectsSection = document.querySelector(".items");
+        projectsSection.appendChild(newProject);
+    
+        // Fermez la modal
+        modal.style.display = "none";
+    });
+    
+  });
+  
